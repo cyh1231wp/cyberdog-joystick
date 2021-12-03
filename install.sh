512c91ae1b1e014c83f195a1c88e4df87729b4db
cyh1231wp <cyh1231wp@163.com>
2021-12-03 23:45:53 +0800

    修改环境变量实现自启动

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
-sudo pip install grpcio
-sudo pip install grpcio-tools
+# sudo pip install grpcio
+# sudo pip install grpcio-tools
 sudo cp joystick.service /lib/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl start joystick
 sudo systemctl enable joystick
+# sudo cp init /etc/init.d/joystick
+# sudo chmod 777 /etc/init.d/joystick
+# sudo update-rc.d joystick defaults
